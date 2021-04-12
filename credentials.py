c0f1ff76a1fa55cbaefd6181ab928d16eb5e718b
Umesh162 <umeshpalival@gmail.com>
2021-04-12 20:46:08 +0530

    Add token file

---
+++ credentials.py
@@ -0,0 +1,9 @@
+import os
+
+Access_Token=os.environ.get('ACCESS_TOEKN')
+Access_Token_Secret=os.environ.get('ACCESS_TOKEN_SECRET')
+Api_key=os.environ.get('API_KEY')
+Api_secret_key=os.environ.get('API_SECRET_KEY')
+
+
+
