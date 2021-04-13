7ef7e3d0a270f6cb875465a71c562584b258a8e9
Umesh162 <umeshpalival@gmail.com>
2021-04-13 12:02:12 +0530

    Trump name in track word

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-TRACK_WORDS = ['Facebook']
+TRACK_WORDS = ['Trump']
 TABLE_NAME = "Facebook"
 TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
             polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
